e2345c246470a3bfbe228fc1a0ed1e29781f5d53
hellothere402 <archiejohnbruce@outlook.com>
2025-07-21 11:22:08 +1200

    gfdg

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  distDir: 'out',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
